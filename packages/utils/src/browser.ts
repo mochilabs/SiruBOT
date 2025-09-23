7986d728d836d9204774fa48e680b69e05674326
Park Seonu <park.seonu@proton.me>
2025-09-23 22:32:25 +0900

    feat: add browser export support and create browser module

---
+++ packages/utils/src/browser.ts
@@ -0,0 +1,8 @@
+export * from "./version";
+export * from "./constants";
+export * from "./format";
+export * from "./time";
+export * from "./array";
+export * from "./memoryCache";
+
+export { pickRandom, isDev } from "./index";
